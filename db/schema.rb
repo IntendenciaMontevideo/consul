4a8b7410ddd8a324551cef9213ad4d6b3fdeda5a
Gaston <garbeletche@vairix.com>
2017-10-06 21:42:59 -0300

    test

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20171010143623) do
     t.uuid     "visit_id"
     t.integer  "user_id"
     t.string   "name"
-    t.jsonb    "properties"
+    t.json     "properties"
     t.datetime "time"
     t.string   "ip"
   end
@@ -536,7 +536,7 @@ ActiveRecord::Schema.define(version: 20171010143623) do
   create_table "locks", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "tries",        default: 0
-    t.datetime "locked_until", default: '2000-01-01 00:01:01', null: false
+    t.datetime "locked_until", default: '2000-01-01 04:01:01', null: false
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
   end
@@ -1000,7 +1000,7 @@ ActiveRecord::Schema.define(version: 20171010143623) do
     t.boolean  "email_digest",                              default: true
     t.boolean  "email_on_direct_message",                   default: true
     t.boolean  "official_position_badge",                   default: false
-    t.datetime "password_changed_at",                       default: '2015-01-01 00:01:01', null: false
+    t.datetime "password_changed_at",                       default: '2017-10-07 00:36:52', null: false
     t.boolean  "created_from_signature",                    default: false
     t.integer  "failed_email_digests_count",                default: 0
     t.text     "former_users_data_log",                     default: ""
