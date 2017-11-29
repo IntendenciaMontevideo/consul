6caf6e730c7bf8c2c4828293432f291b0a3c0279
vairix-garbeletche <garbeletche@vairix.com>
2017-11-29 14:50:46 +0100

    Fixes issuer and idp_entity_id settings for saml

--- config/initializers/onmiauth.rb
+++ config/initializers/onmiauth.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :saml,
            #:assertion_consumer_service_url     => "consumer_service_url",
-           issuer:                              "ihdesa.imm.gub.uy",
+           issuer:                              "localhost.participa.montevideo.gub.uy",
+           idp_entity_id:                       "ihdesa.imm.gub.uy",
            idp_sso_target_url:                  "https://ihdesa.imm.gub.uy:9443/samlsso",
            idp_slo_target_url:                  "https://ihdesa.imm.gub.uy:9443/samlsso",
            idp_cert:                            "-----BEGIN CERTIFICATE-----\nMIICNTCCAZ6gAwIBAgIES343gjANBgkqhkiG9w0BAQUFADBVMQswCQYDVQQGEwJVUzELMAkGA1UE'\nCAwCQ0ExFjAUBgNVBAcMDU1vdW50YWluIFZpZXcxDTALBgNVBAoMBFdTTzIxEjAQBgNVBAMMCWxv\nY2FsaG9zdDAeFw0xMDAyMTkwNzAyMjZaFw0zNTAyMTMwNzAyMjZaMFUxCzAJBgNVBAYTAlVTMQsw\nCQYDVQQIDAJDQTEWMBQGA1UEBwwNTW91bnRhaW4gVmlldzENMAsGA1UECgwEV1NPMjESMBAGA1UE\nAwwJbG9jYWxob3N0MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCUp/oV1vWc8/TkQSiAvTou\nsMzOM4asB2iltr2QKozni5aVFu818MpOLZIr8LMnTzWllJvvaA5RAAdpbECb+48FjbBe0hseUdN5\nHpwvnH/DW8ZccGvk53I6Orq7hLCv1ZHtuOCokghz/ATrhyPq+QktMfXnRS4HrKGJTzxaCcU7OQID\nAQABoxIwEDAOBgNVHQ8BAf8EBAMCBPAwDQYJKoZIhvcNAQEFBQADgYEAW5wPR7cr1LAdq+IrR44i\nQlRG5ITCZXY9hI0PygLP2rHANh+PYfTmxbuOnykNGyhM6FjFLbW2uZHQTY1jMrPprjOrmyK5sjJR\nO4d1DeGHT/YnIjs9JogRKv4XHECwLtIVdAbIdWHEtVZJyMSktcyysFcvuhPQK8Qc/E/Wq8uHSCo=\n-----END CERTIFICATE------",
