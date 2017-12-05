3e60bf9bb2765314e2d9e823391e5a9754ee4932
Gaston <garbeletche@vairix.com>
2017-12-04 23:07:24 -0300

    add last_name_2, add verified to connect saml with correct attr

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171201104457) do
+ActiveRecord::Schema.define(version: 20171205015836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1057,6 +1057,7 @@ ActiveRecord::Schema.define(version: 20171201104457) do
     t.string   "document"
     t.boolean  "user_verified"
     t.string   "middle_name"
+    t.string   "last_name_2"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
