55db5bdda61449dee54c3cf429c1e10d03e96aff
Gastón Arbeletche <garbeletche@vairix.com>
2018-02-08 22:44:41 -0300

    Feature/add tag analytics (#40)
    * add script facebook

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -8,4 +8,9 @@ module SettingsHelper
     @all_settings ||= Hash[ Setting.all.map{|s| [s.key, s.value.presence]} ]
   end
 
+  def show_in_production
+    value_show = setting["show_in_production"]
+    value_show && (value_show == 'true' || value_show == 't')
+  end
+
 end
