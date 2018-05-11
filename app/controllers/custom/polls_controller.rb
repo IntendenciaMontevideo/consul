6e3918afc60b3465d0ac3f38c6e96c8a7de64280
Gaston Arbeletche <garbeletche@vairix.com>
2018-05-11 15:56:17 -0300

    to share only a question

---
+++ app/controllers/custom/polls_controller.rb
@@ -0,0 +1,57 @@
+class PollsController < ApplicationController
+  include PollsHelper
+
+  load_and_authorize_resource
+
+  has_filters %w{current expired incoming}
+  has_orders %w{most_voted newest oldest}, only: [:show, :show_question]
+
+  ::Poll::Answer # trigger autoload
+
+  def index
+    @polls = @polls.send(@current_filter).includes(:geozones).sort_for_list.page(params[:page])
+  end
+
+  def show
+    @is_show_question = false
+    @questions = @poll.questions.for_render.sort_for_list
+    @token = poll_voter_token(@poll, current_user)
+    @poll_questions_answers = Poll::Question::Answer.where(question: @poll.questions).where.not(description: "").order(:given_order)
+
+    @answers_by_question_id = {}
+    poll_answers = ::Poll::Answer.by_question(@poll.question_ids).by_author(current_user.try(:id))
+    poll_answers.each do |answer|
+      @answers_by_question_id[answer.question_id] = answer.answer
+    end
+
+    @commentable = @poll
+    @comment_tree = CommentTree.new(@commentable, params[:page], @current_order)
+  end
+
+  def show_question
+    @is_show_question = true
+    @questions = [Poll::Question.find(params[:question].to_i)]
+    @token = poll_voter_token(@poll, current_user)
+    @poll_questions_answers = Poll::Question::Answer.where(question: @poll.questions).where.not(description: "").order(:given_order)
+
+    @answers_by_question_id = {}
+    poll_answers = ::Poll::Answer.by_question(@questions.first.id).by_author(current_user.try(:id))
+    poll_answers.each do |answer|
+      @answers_by_question_id[answer.question_id] = answer.answer
+    end
+
+    @commentable = @poll
+    @comment_tree = CommentTree.new(@commentable, params[:page], @current_order)
+
+    render 'show'
+
+  end
+
+  def stats
+    @stats = Poll::Stats.new(@poll).generate
+  end
+
+  def results
+  end
+
+end
--- config/routes/poll.rb
+++ config/routes/poll.rb
@@ -2,6 +2,7 @@ resources :polls, only: [:show, :index] do
   member do
     get :stats
     get :results
+    get :show_question
   end
 
   resources :questions, controller: 'polls/questions', shallow: true do
