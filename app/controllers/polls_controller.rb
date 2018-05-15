a6d4516a96c8df69368254a91cacdadd53d2d56c
Gaston Arbeletche <garbeletche@vairix.com>
2018-05-15 10:35:39 -0300

    delete folder custom on controllers

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -4,7 +4,7 @@ class PollsController < ApplicationController
   load_and_authorize_resource
 
   has_filters %w{current expired incoming}
-  has_orders %w{most_voted newest oldest}, only: :show
+  has_orders %w{most_voted newest oldest}, only: [:show, :show_question]
 
   ::Poll::Answer # trigger autoload
 
@@ -13,6 +13,7 @@ class PollsController < ApplicationController
   end
 
   def show
+    @is_show_question = false
     @questions = @poll.questions.for_render.sort_for_list
     @token = poll_voter_token(@poll, current_user)
     @poll_questions_answers = Poll::Question::Answer.where(question: @poll.questions).where.not(description: "").order(:given_order)
@@ -25,6 +26,25 @@ class PollsController < ApplicationController
 
     @commentable = @poll
     @comment_tree = CommentTree.new(@commentable, params[:page], @current_order)
+  end
+
+  def show_question
+    @is_show_question = true
+    @questions = [Poll::Question.find(params[:question].to_i)]
+    @token = poll_voter_token(@poll, current_user)
+    @poll_questions_answers = Poll::Question::Answer.where(question: @poll.questions).where.not(description: "").order(:given_order)
+
+    @answers_by_question_id = {}
+    poll_answers = ::Poll::Answer.by_question(@questions.first.id).by_author(current_user.try(:id))
+    poll_answers.each do |answer|
+      @answers_by_question_id[answer.question_id] = answer.answer
+    end
+
+    @commentable = @poll
+    @comment_tree = CommentTree.new(@commentable, params[:page], @current_order)
+
+    render 'show'
+
   end
 
   def stats
