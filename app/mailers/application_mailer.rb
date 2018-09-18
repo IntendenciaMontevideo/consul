2e649a961a01e3368ce11e70f8ca09d622c1efed
Gaston Arbeletche <garbeletche@vairix.com>
2018-09-18 17:00:43 -0300

    add inline attachment in the layout mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,11 @@ class ApplicationMailer < ActionMailer::Base
   default from: "#{Setting['mailer_from_name']} <#{Setting['mailer_from_address']}>"
   layout 'mailer'
   layout false, :only => 'welcome_email'
+
+  before_filter :add_inline_attachments!
+
+  private
+  def add_inline_attachments!
+    attachments.inline['logo_email.png'] = File.read("#{Rails.root}/app/assets/images/logo_email.png")
+  end
 end
