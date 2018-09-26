07809231aac98c2f6eb55d377b33e3d3df38e552
Gaston Arbeletche <garbeletche@vairix.com>
2018-09-26 10:59:53 -0300

    update email ticket vote

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,12 +2,12 @@ class ApplicationMailer < ActionMailer::Base
   helper :settings
   default from: "#{Setting['mailer_from_name']} <#{Setting['mailer_from_address']}>"
   layout 'mailer'
-  layout false, :only => 'welcome_email'
+  layout false, :only => [:welcome_email, :email_ticket_vote]
 
   before_filter :add_inline_attachments!
 
   private
   def add_inline_attachments!
-    attachments.inline['logo_email.png'] = File.read("#{Rails.root}/app/assets/images/logo_email.png")
+    attachments.inline['header_email.png'] = File.read("#{Rails.root}/app/assets/images/custom/header_email.png")
   end
 end
