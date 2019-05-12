352cf0b40c242c9bf79902bb1d73126df6558522
Gaston <garbeletche@vairix.com>
2019-05-12 20:34:02 -0300

    change home

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,9 @@ class WelcomeController < ApplicationController
   def index
     @recommendations = Widget::Card.body.limit(3)
     @headers = Widget::Card.header
+    @recommendation_is_active = recomendation_home?
+    @feed_proposal = Widget::Feed.find_by_kind(Widget::Feed::KINDS[0])
+    @feed_debate = Widget::Feed.find_by_kind(Widget::Feed::KINDS[1])
   end
 
   def welcome
@@ -21,6 +24,11 @@ class WelcomeController < ApplicationController
   def set_user_recommendations
     @recommended_debates = Debate.recommendations(current_user).sort_by_recommendations.limit(3)
     @recommended_proposals = Proposal.recommendations(current_user).sort_by_recommendations.limit(3)
+  end
+
+  def recomendation_home?
+    recomendation = Setting.find_by_key("feature.user.recommendations")
+    recomendation && recomendation.value == "active"
   end
 
 end
